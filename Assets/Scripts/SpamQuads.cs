4e637624069bbb5af032778bc0a6d7acfb1a2ddb
Brandon Farrell <Brandon@BPFarrell.com>
2018-01-27 10:03:43 -0800

    SlabSpawner

--- Assets/Scripts/SpamQuads.cs
+++ Assets/Scripts/SpamQuads.cs
@@ -9,32 +9,7 @@ public class SpamQuads : MonoBehaviour {
 	void Start () {
         GameObject parent = new GameObject("Parent");
         for (int y = 0; y < 40; y++) {
-            Vector2[] pos = new Vector2[4];
-            for(int uv = 0; uv < 4; uv++) {
-                pos[uv] = new Vector2(y, y);
-            }
-            for (int x = 0; x < width; x++) {
-                GameObject go = GameObject.CreatePrimitive(PrimitiveType.Quad);
-                go.transform.position = new Vector3(x, 0, y);
-                go.transform.eulerAngles = new Vector3(90, 0, 0);
-                go.GetComponent<MeshRenderer>().material = mat;
-                Mesh mesh = go.GetComponent<MeshFilter>().mesh;
-                float xPos = ((float)x) / (width-1);
-                Color[] clrs = new Color[4];
-                for (int uv = 0; uv < 4; uv++) {
-                    pos[uv].x = xPos;
-                    Color clr = new Color(
-                        Random.Range(-1f, 1f),
-                        Random.Range(-1f, 1f),
-                        Random.Range(-1f, 1f));
-                    clrs[uv] = clr;
-                }
-                mesh.colors = clrs;
-                mesh.uv2 = pos;
-                mesh.UploadMeshData(false);
-                go.GetComponent<MeshFilter>().mesh = mesh;
-                go.transform.SetParent(parent.transform);
-            }
+            SlabController.SpawnSlab(new Vector3(0, 0, y), Vector3.zero, y);
         }
     }
 	
