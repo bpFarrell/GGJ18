1a3d4f1304265e299cf300f1bc8e5a26d2402c6f
vachara <vakara@hotmail.com>
2018-01-27 11:18:47 -0800

    race root obj detects rear ray cast

--- Assets/Scripts/RaceObjBehave.cs
+++ Assets/Scripts/RaceObjBehave.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿#define CAN_FALL
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -26,30 +27,40 @@ public class RaceObjBehave : MonoBehaviour {
         
         Vector3 direction = transform.forward * Time.deltaTime * speed;
         Vector3 up = transform.up;
-        Ray ray = new Ray(transform.position + (transform.up * 10) + transform.forward*2, -transform.up);
+        Ray ray = new Ray(transform.position + (transform.up * 10) + transform.forward*racer.transform.localScale.z*2, -transform.up);
+        Ray backRay = new Ray(transform.position + transform.up * 10 + transform.forward * racer.transform.localScale.z* - 1.5f, -transform.up);
         RaycastHit hitInfo;
-        Debug.DrawRay(ray.origin, ray.direction * 10);
-        if (Physics.Raycast(ray, out hitInfo))
+        Debug.DrawRay(ray.origin, ray.direction * 10,Color.green);
+        Debug.DrawRay(backRay.origin, backRay.direction * 10,Color.yellow);
+#if CAN_FALL
+        if (Physics.Raycast(ray, out hitInfo)||Physics.Raycast(backRay))
         {
-            if (hitInfo.transform.gameObject.layer == 4)
+            if (hitInfo.transform != null)
             {
-                state = State.onTrack;
-                if (Input.GetKey(KeyCode.A))
+#else
+        if (Physics.Raycast(ray, out hitInfo) || Physics.Raycast(backRay, out hitInfo))
+        {
+#endif
+            if (hitInfo.transform.gameObject.layer == 4)
                 {
-                    transform.Rotate(Vector3.up * -100 * Time.deltaTime);
+                    state = State.onTrack;
+                    if (Input.GetKey(KeyCode.A))
+                    {
+                        transform.Rotate(Vector3.up * -100 * Time.deltaTime);
+                    }
+                    if (Input.GetKey(KeyCode.D))
+                    {
+                        transform.Rotate(Vector3.up * 100 * Time.deltaTime);
+                    }
+                    Vector3 point = hitInfo.point;
+                    transform.position = Vector3.Lerp(transform.position, point, Time.deltaTime * speed);
+                    up = hitInfo.normal;
                 }
-                if (Input.GetKey(KeyCode.D))
-                {
-                    transform.Rotate(Vector3.up * 100 * Time.deltaTime);
-                }
-                Vector3 point = hitInfo.point;
-                transform.position = Vector3.Lerp(transform.position, point, Time.deltaTime* speed);
-                up = hitInfo.normal;
             }
         }
         else {
             state = State.falling;
-            direction += transform.up * -Time.deltaTime * speed * 9.8f;
+            direction += transform.up * -Time.deltaTime * 9.8f;
         }
         
         transform.position += direction;
