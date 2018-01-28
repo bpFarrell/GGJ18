f84496777233d84738a799c49b6e4c7c62f43f92
DESKTOP-4CPT7KV\rojib <jasondeancrossley@gmail.com>
2018-01-27 22:14:26 -0800

    Countdown complete and packaged spline fix for z scale through default values trackbuilder is in an ok spot for generation

---
+++ Assets/Scripts/Editor/CountDownEditor.cs
@@ -0,0 +1,45 @@
+﻿/*
+////////////////////////////////////////////////////////////////////
+ Jason Dean Crossley
+
+ Notes:
+\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(CountDown))]
+public class CountDownEditor : Editor {
+    private bool debug = true;
+
+    SerializedProperty sprites;
+
+    void OnEnable()
+    {
+        sprites = serializedObject.FindProperty("sprites");
+    }
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+        EditorGUILayout.PropertyField(sprites, true);
+
+        debug = EditorGUILayout.Foldout(debug, "Debug");
+        if (debug)
+        {
+            // Function Testing
+            if (GUILayout.Button("Reset and Start"))
+            {
+                (target as CountDown).SetAndCount();
+            }
+            // Variable Display
+            EditorGUI.BeginDisabledGroup(true);
+
+            EditorGUI.EndDisabledGroup();
+        }
+        serializedObject.ApplyModifiedProperties();
+    }
+	
+}
