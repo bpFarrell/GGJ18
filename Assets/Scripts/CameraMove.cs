1573468f0429221c66c867641f026237d4f77de5
vachara <vakara@hotmail.com>
2018-01-28 05:28:17 -0800

    camera looks ahead of player from higher position

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CameraMove : MonoBehaviour {
     public float distBack = 7;
-    public float distUp = 4;
+    public float distUp = 8;
     public Transform racer;
     public float speed = 7;
     Vector3 anchor {
@@ -14,7 +14,8 @@ public class CameraMove : MonoBehaviour {
     }
 
 	void Update () {
-        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation((racer.position - transform.position),Vector3.up), Time.deltaTime*speed);
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(((racer.position+racer.forward*5) - transform.position),Vector3.up), Time.deltaTime*speed);
         transform.position = Vector3.Lerp(transform.position, anchor, Time.deltaTime * speed);
 	}
 }
