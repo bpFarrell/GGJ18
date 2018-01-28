22beae41cf66b0b2200fa9313240f91eada3f1fc
DESKTOP-4CPT7KV\rojib <jasondeancrossley@gmail.com>
2018-01-28 02:57:30 -0800

    Credits script for controlling last scene

---
+++ Assets/Scripts/Credits/EndCredits.cs
@@ -0,0 +1,41 @@
+﻿/*
+////////////////////////////////////////////////////////////////////
+ Jason Dean Crossley
+
+ Notes:
+\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+*/
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndCredits : MonoBehaviour {
+    public RectTransform credits;
+    public float speed;
+
+    private bool isDone = false;
+
+    private void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (isDone) return;
+        credits.anchoredPosition += Vector2.up * (Time.deltaTime * speed);
+        if(credits.anchoredPosition.y >= 0f)
+        {
+            CreditsOver();
+            isDone = true;
+        }
+    }
+
+    private void CreditsOver()
+    {
+        //throw new NotImplementedException();
+    }
+}
